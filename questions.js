481dae544f1a02e81d043f84855ccbcb44b74367
Daniel Gendik <gendik38@gmail.com>
2022-11-16 16:38:13 +0100

    adding questions.js

---
+++ questions.js
@@ -0,0 +1,24 @@
+const questions = [
+    {
+        question:'What is the capital of France ?',
+        options:['Paris', 'Madrid', 'Strasbourg', 'Cologne'],
+        answer:0
+        },
+        {
+            question:'How many buttons or keys has an ordinary english keyboard ? ',
+            options:[92, 104, 54, 72],
+            answer:1
+            },
+            {
+                question: 'In which year was JavaScript introduced ?',
+                options:[2000, 1999, 1995, 1996],
+                answer:2
+                },
+                {
+                    question: 'Which of these languages is only useful for frontend development ? ',
+                    options:['HTML', 'JavaScript', 'Python', 'C#'],
+                    answer:2
+                    },
+
+
+];
