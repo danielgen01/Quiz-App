239752f6882a993f018aeda90849ab236a4390b3
Daniel Gendik <gendik38@gmail.com>
2022-12-02 12:30:53 +0100

    removing small typo in q2

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@
   
 const q1 = 
 {
-    question:'Which key number hast the "enter" in JavaScript events ? ',
+    question:'Which key number has the "enter" in JavaScript events ? ',
     options:[13, 11, 7, 1],
     answer:0
     };
